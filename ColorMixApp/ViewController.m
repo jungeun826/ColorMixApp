b5c9bf06bde07305beeb37e6a2f86d2f9bd3ba3e
jungeun826 <jungeun826@nate.com>
2014-01-03 16:20:37 +0900

    ColorMix 실습 완료했습니다.
    나누어준 책의 UI만 보고 작성했다.
    책에 있는 내용도 숙지하도록 하자.

--- ColorMixApp/ViewController.m
+++ ColorMixApp/ViewController.m
@@ -8,16 +8,59 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController (){
+    float r;
+    float g;
+    float b;
+    UIActivityIndicatorView *indicatorCode;
+}
+@property (weak, nonatomic) IBOutlet UIView *colorView;
+- (IBAction)changeColor:(UISlider *)sender;
 @end
 
 @implementation ViewController
+- (IBAction)switchOnOff:(UISwitch *)sender {
+    UISlider *red = self.view.subviews[0];
+    UISlider *green = self.view.subviews[1];
+    UISlider *blue = self.view.subviews[2];
+    if(sender.on){
+        self.colorView.backgroundColor = [UIColor grayColor];
+        [indicatorCode performSelector:@selector(stopAnimating) withObject:nil afterDelay:0.2];
+        red.enabled = YES;
+        green.enabled = YES;
+        blue.enabled = YES;
+    }else{
+        self.colorView.backgroundColor = [UIColor colorWithRed:r green:g blue:b alpha:1.0];
+        [indicatorCode performSelector:@selector(startAnimating) withObject:nil afterDelay:0.2];
+        red.enabled = NO;
+        green.enabled = NO;
+        blue.enabled = NO;
+    }
+    
+}
+- (IBAction)changeColor:(UISlider *)sender {
+    if(sender.tag == 0)
+        r = sender.value;
+    else if(sender.tag == 1)
+        g = sender.value;
+    else if(sender.tag ==2)
+        b = sender.value;
+    
+    self.colorView.backgroundColor = [UIColor colorWithRed:r green:g blue:b alpha:1.0];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    r = 0.5f;
+    g = 0.5f;
+    b = 0.5f;
 	// Do any additional setup after loading the view, typically from a nib.
+    indicatorCode = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    indicatorCode.hidesWhenStopped = YES;
+    indicatorCode.center = CGPointMake(156, 436);
+    
+    [self.view addSubview:indicatorCode];
 }
 
 - (void)didReceiveMemoryWarning

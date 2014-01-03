9049a0afc7ea401ae4759a5a158c91e9c8090063
jungeun826 <jungeun826@nate.com>
2014-01-03 16:27:41 +0900

    주석 추가했습니다.

--- ColorMixApp/ViewController.m
+++ ColorMixApp/ViewController.m
@@ -9,12 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController (){
+    //view에 반영할 r,g,b값
     float r;
     float g;
     float b;
-    UIActivityIndicatorView *indicatorCode;
 }
 @property (weak, nonatomic) IBOutlet UIView *colorView;
+
+//추가한 indicator에 대한 정보 저장
+@property (strong, nonatomic) UIActivityIndicatorView *indicatorCode;
 - (IBAction)changeColor:(UISlider *)sender;
 @end
 
@@ -23,15 +26,24 @@
     UISlider *red = self.view.subviews[0];
     UISlider *green = self.view.subviews[1];
     UISlider *blue = self.view.subviews[2];
+    //switch에 따라 다른 행동
+    //on
+    //1.view의 backgroundColor값을 회색으로 변경
+    //2.activate indicator stop
+    //3.r,g,b enable
     if(sender.on){
         self.colorView.backgroundColor = [UIColor grayColor];
-        [indicatorCode performSelector:@selector(stopAnimating) withObject:nil afterDelay:0.2];
+        [self.indicatorCode performSelector:@selector(stopAnimating) withObject:nil afterDelay:0.2];
         red.enabled = YES;
         green.enabled = YES;
         blue.enabled = YES;
-    }else{
+    }else{ß
+        //off
+        //1.view의 backgroundColor값을 r,g,b로 변경
+        //2.activate indicator start
+        //3.r,g,b disable
         self.colorView.backgroundColor = [UIColor colorWithRed:r green:g blue:b alpha:1.0];
-        [indicatorCode performSelector:@selector(startAnimating) withObject:nil afterDelay:0.2];
+        [self.indicatorCode performSelector:@selector(startAnimating) withObject:nil afterDelay:0.2];
         red.enabled = NO;
         green.enabled = NO;
         blue.enabled = NO;
@@ -39,11 +51,12 @@
     
 }
 - (IBAction)changeColor:(UISlider *)sender {
-    if(sender.tag == 0)
+    //들어온 sender의 tag에 따라 r,g,b 저장
+    if(sender.tag == 0) //red slider
         r = sender.value;
-    else if(sender.tag == 1)
+    else if(sender.tag == 1)//green slider
         g = sender.value;
-    else if(sender.tag ==2)
+    else if(sender.tag ==2)//blue slider
         b = sender.value;
     
     self.colorView.backgroundColor = [UIColor colorWithRed:r green:g blue:b alpha:1.0];
@@ -52,15 +65,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //slider초기값을 r,g,bd에 저장
     r = 0.5f;
     g = 0.5f;
     b = 0.5f;
 	// Do any additional setup after loading the view, typically from a nib.
-    indicatorCode = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-    indicatorCode.hidesWhenStopped = YES;
-    indicatorCode.center = CGPointMake(156, 436);
+    self.indicatorCode = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    self.indicatorCode.hidesWhenStopped = YES;
+    self.indicatorCode.center = CGPointMake(156, 436);
     
-    [self.view addSubview:indicatorCode];
+    [self.view addSubview:self.indicatorCode];
 }
 
 - (void)didReceiveMemoryWarning
